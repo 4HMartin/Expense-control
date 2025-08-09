fdb227babfbf1fad9d3b387c96dd5ecbcc85ff6c
Heber <parzival.html@gmail.com>
2025-08-09 17:36:30 +0200

    Showing the expenses.

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,5 +1,26 @@
-export default function ExpenseDetail(){
+import { formatDate } from "../helpers"
+import type { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
+
+type ExpenseDetailProps = {
+    expense: Expense
+}
+
+export default function ExpenseDetail({ expense }: ExpenseDetailProps){
     return (
-        <></>
+        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200">
+            <div>
+
+            </div>
+
+            <div>
+                <p>{expense.expenseName}</p>
+                <p className="text-slate-600 text-sm">{ formatDate(expense.date!.toString()) }</p>
+            </div>
+
+            <AmountDisplay
+                amount={expense.amount}
+            />
+        </div>
     )
 }
