ee8f03f4a23a26d9c250d2393e56a8317735fd9c
Heber <parzival.html@gmail.com>
2025-08-11 15:31:35 +0200

    Remove a expense when executing a trailing swipe.

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -10,6 +10,7 @@ import { categories } from "../data/categories"
 import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { useBudget } from "../hooks/useBudget"
 import "react-swipeable-list/dist/styles.css"
 
 type ExpenseDetailProps = {
@@ -17,6 +18,8 @@ type ExpenseDetailProps = {
 }
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps){
+
+    const { dispatch } = useBudget()
 
     const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
 
@@ -33,7 +36,7 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps){
     const trailingActions = () => (
         <TrailingActions>
             <SwipeAction
-                onClick={() => {}}
+                onClick={() => dispatch({ type: 'remove-expense', payload: { id: expense.id }})}
                 destructive={true}
             >
                 Delete
